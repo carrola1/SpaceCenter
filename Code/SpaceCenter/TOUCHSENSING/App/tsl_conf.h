3605fc0db6fd2ec252d07716eb5359b377824b6e
Andrew <carrola1@gmail.com>
2021-08-12 20:33:50 -0400

    All touch channels functioning. Need to add offset mapping.
    Channel 0 is still more sensitive than the rest.

--- Code/SpaceCenter/TOUCHSENSING/App/tsl_conf.h
+++ Code/SpaceCenter/TOUCHSENSING/App/tsl_conf.h
@@ -158,18 +158,18 @@
 /** TouchKeys Detect state input threshold (range=0..255)
   - Enter Detect state if delta is above
 */
-#define TSLPRM_TKEY_DETECT_IN_TH (30)
+#define TSLPRM_TKEY_DETECT_IN_TH (20)
 
 /** TouchKeys Detect state output threshold (range=0..255)
   - Exit Detect state if delta is below
 */
-#define TSLPRM_TKEY_DETECT_OUT_TH (20)
+#define TSLPRM_TKEY_DETECT_OUT_TH (10)
 
 /** TouchKeys re-Calibration threshold (range=0..255)
   - @warning The value is inverted in the sensor state machine
   - Enter Calibration state if delta is below
 */
-#define TSLPRM_TKEY_CALIB_TH (10)
+#define TSLPRM_TKEY_CALIB_TH (7)
 
 /** TouchKey, Linear and Rotary sensors thresholds coefficient (range=0..4)
     This multiplier coefficient is applied on Detect and Re-Calibration thresholds only.
